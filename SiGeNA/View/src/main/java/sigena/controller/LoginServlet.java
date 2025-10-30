8c1c2122cb8aacfd08c2cd0c81d2453b9480d8b4
Tiago160109 <tiagopereira16012009@gmail.com>
2025-10-30 16:43:26 -0300

    COmmitando o caso de uso de login #18

---
+++ SiGeNA/View/src/main/java/sigena/controller/LoginServlet.java
@@ -0,0 +1,50 @@
+package sigena.controller;
+
+import java.io.IOException;
+import sigena.model.UsuarioDAO;
+import java.io.PrintWriter;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+           
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String cpf = request.getParameter("cpf").trim();
+        String senha = request.getParameter("senha").trim();
+         System.out.println("Tentando login - CPF: '" + cpf + "' Senha: '" + senha + "'");
+        UsuarioDAO dao = new UsuarioDAO();
+        if(dao.autenticar(cpf, senha)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("CpfLogado", cpf);
+            response.sendRedirect("home.jsp");
+         
+        } 
+        else {
+            request.setAttribute("erro", "Cpf ou senha inválidos!");
+            RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+            rd.forward(request, response);
+        }
+            
+    }
+}

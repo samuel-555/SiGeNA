e525dddfc2149ae34bb2d30e967c3d2f81c21332
saraeam <saraesteraaaa@gmail.com>
2025-11-20 21:46:11 -0300

    Primeira versao da calsse de dominio de produtos #11

---
+++ SiGeNA/Model/src/main/java/sigena/model/domain/Produto.java
@@ -0,0 +1,70 @@
+package sigena.model.domain;
+
+import java.time.LocalDate;
+
+public class Produto {
+
+    public String nome;
+    public Long id;
+    //public Fornecedor fornecedor;
+    public int quantidade;
+    public LocalDate validade;
+    public LocalDate lote;
+    public Boolean disponivel;
+
+    public Produto(String nome, int quantidade, LocalDate validade, LocalDate lote, Boolean disponivel) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.validade = validade;
+        this.lote = lote;
+        this.disponivel = disponivel;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setValidade(LocalDate validade) {
+        this.validade = validade;
+    }
+
+    public LocalDate getValidade() {
+        return validade;
+    }
+
+    public void setLote(LocalDate lote) {
+        this.lote = lote;
+    }
+
+    public LocalDate getLote() {
+        return lote;
+    }
+
+    public void setDisponivel(Boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+}
